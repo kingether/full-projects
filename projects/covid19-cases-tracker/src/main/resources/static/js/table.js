196c5c7bb660eed871020e69fed692d2ccad0343
Abel Morris Ben <abelben59@gmail.com>
2020-10-12 15:35:18 +0200

    covid 19 tracker

---
+++ projects/covid19-cases-tracker/src/main/resources/static/js/table.js
@@ -0,0 +1,5 @@
+$(document).ready( function () {
+	$('table').dataTable();//{searching: true, paging: true, info: true});
+} );
+
+//$('table').dataTable({searching: false, paging: false, info: false});
