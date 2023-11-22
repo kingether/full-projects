b22d86427bd7fa7d64697f6b893e41654eb484ff
kingether <50985664+kingether@users.noreply.github.com>
2023-11-22 13:54:24 +0200

    Delete chat directory

--- chat/Meeting-Reservation-master/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar target/meeting-reservation-0.0.1-SNAPSHOT.jar
--- chat/two-factor-authentication-master/auth-service/src/main/java/com/clone/instagram/authservice/payload/VerifyCodeRequest.java
+++
@@ -1,9 +0,0 @@
-package com.clone.instagram.authservice.payload;
-
-import lombok.Data;
-
-@Data
-public class VerifyCodeRequest {
-    private String username;
-    private String code;
-}
