0f0e07149b0dbc5831b511c697730d701cacda70
kingether <50985664+kingether@users.noreply.github.com>
2023-11-22 13:56:49 +0200

    Delete payment directory

--- payment/online-bookstore-master/src/main/java/com/bookstore/onlinebookstore/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore.onlinebookstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookstore.onlinebookstore.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
