07497089981b00b7fd76b2070ba25c16e4d1dd6c
kingether <50985664+kingether@users.noreply.github.com>
2023-11-22 13:55:24 +0200

    Delete insta-clone directory

--- insta-clone/my-moments-master.zip_expanded/my-moments-master/insta-feed-service/src/main/java/com/clone/instagram/instafeedservice/exception/UnableToGetFollowersException.java
+++
@@ -1,6 +0,0 @@
-package com.clone.instagram.instafeedservice.exception;
-
-public class UnableToGetFollowersException extends RuntimeException {
-
-    public UnableToGetFollowersException(String message) {super(message);}
-}
--- insta-clone/my-moments-master.zip_expanded/my-moments-master/insta-feed-service/src/main/java/com/clone/instagram/instafeedservice/exception/UnableToGetPostsException.java
+++
@@ -1,8 +0,0 @@
-package com.clone.instagram.instafeedservice.exception;
-
-public class UnableToGetPostsException extends RuntimeException {
-
-    public UnableToGetPostsException(String message) {
-        super(message);
-    }
-}
