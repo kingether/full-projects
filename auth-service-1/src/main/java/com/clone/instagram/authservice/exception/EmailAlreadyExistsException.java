ed87037fc566b36c2e4c578e00c321a4427121be
kingether <50985664+kingether@users.noreply.github.com>
2023-11-22 13:53:37 +0200

    Delete auth-service-1 directory

--- auth-service-1/src/main/java/com/clone/instagram/authservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.clone.instagram.authservice.exception;
-
-
-public class EmailAlreadyExistsException extends RuntimeException {
-
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
